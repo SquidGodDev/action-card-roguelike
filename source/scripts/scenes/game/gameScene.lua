296c5203a521413be13c93b893e75d8747be78ab
SquidGodDev <tako48188@gmail.com>
2023-05-09 15:45:37 -0700

    Added .pdx to gitignore

--- source/scripts/scenes/game/gameScene.lua
+++ source/scripts/scenes/game/gameScene.lua
@@ -6,15 +6,16 @@ local sceneManager = SceneManager
 GameScene = {}
 
 function GameScene.init()
-    gfx.setBackgroundColor(gfx.kColorBlack)
+    gfx.sprite.setBackgroundDrawingCallback(function()
+        gfx.setColor(gfx.kColorBlack)
+        gfx.fillRect(0, 0, 400, 240)
+    end)
 end
 
 function GameScene.update()
     if pd.buttonJustPressed(pd.kButtonA) then
         sceneManager.switchScene(TitleScene)
     end
-    gfx.setColor(gfx.kColorBlack)
-    gfx.fillRect(0, 0, 400, 240)
     gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
     gfx.drawTextAligned("GameScene", 200, 120, kTextAlignment.center)
 end
--- source/scripts/scenes/title/titleScene.lua
+++ source/scripts/scenes/title/titleScene.lua
@@ -6,15 +6,16 @@ local sceneManager = SceneManager
 TitleScene = {}
 
 function TitleScene.init()
-    gfx.setBackgroundColor(gfx.kColorWhite)
+    gfx.sprite.setBackgroundDrawingCallback(function()
+        gfx.setColor(gfx.kColorBlack)
+        gfx.fillRect(0, 0, 400, 240)
+    end)
 end
 
 function TitleScene.update()
     if pd.buttonJustPressed(pd.kButtonA) then
         sceneManager.switchScene(GameScene)
     end
-    gfx.setColor(gfx.kColorBlack)
-    gfx.fillRect(0, 0, 400, 240)
     gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
     gfx.drawTextAligned("Title Scene", 200, 120, kTextAlignment.center)
 end
